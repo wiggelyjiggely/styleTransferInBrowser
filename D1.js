e1716e5203db25e11408a57fe94cfe8334082339
wiggelyjiggely <wiggelyjiggely@hotmail.com>
2020-02-13 13:13:25 +0100

    d

--- D1.js
+++ D1.js
@@ -1,7 +1,21 @@
-var loadContentImage= function(event){
-    var cImg = $('#contimg').get(0)
+var loadContentImage= function(event,imgid,canvasid){
+    var cImg = $(imgid).get(0)
     cImg.src = URL.createObjectURL(event.target.files[0])
     cImg.onload = function(){
-
+        changeCanvasSize(imgid, canvasid)
     }
+}
+function changeCanvasSize(img, overlay){
+    const myImg = $(img).get(0)
+    const canvas = $(overlay).get(0)
+    const {width, height} = myImg
+    canvas.width = width
+    canvas.height = height
+    if (document.getElementById('first').offsetHeight < height+20) {
+        document.getElementById('first').style.height = (height + 20).toString() + "px"
+    }
+}
+
+function transfer(){
+    startStyling(document.getElementById("contimg"),document.getElementById("styleimg") );
 }
--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-import 'babel-polyfill';
-import * as tf from '@tensorflow/tfjs';
+//import 'babel-polyfill';
+//import * as tf from '@tensorflow/tfjs';
 tf.ENV.set('WEBGL_PACK', false);  // This needs to be done otherwise things run very slow v1.0.4
 
 class Main{
     constructor(){
-    Promise.all([
-        loadMobileNetStyleModel(),
-        loadSeparableTransformerModel(),
-      ]).then(([styleNet, transformNet]) => {
-        console.log('Loaded styleNet');
-        this.styleNet = styleNet;
-        this.transformNet = transformNet;
-      });
-}
+        Promise.all([
+            loadMobileNetStyleModel(),
+            loadSeparableTransformerModel(),
+        ]).then(([styleNet, transformNet]) => {
+            console.log('Loaded styleNet');
+            this.styleNet = styleNet;
+            this.transformNet = transformNet;
+        });
+    }
 
-async  loadMobileNetStyleModel() {
-    
-    mobileStyleNet = await tf.loadGraphModel('./models/saved_model_style_js/model.json');
-    return mobileStyleNet;
-}
-
-async  loadInceptionStyleModel() {
-    
-    inceptionStyleNet = await tf.loadGraphModel('./models/saved_model_style_inception_js/model.json');
-    return inceptionStyleNet;
-}
-
-async  loadOriginalTransformerModel() {
-    
-    originalTransformNet = await tf.loadGraphModel('./models/saved_model_transformer_js/model.json');
-    return originalTransformNet;
-}
-
-async  loadSeparableTransformerModel() {
+    async  loadMobileNetStyleModel() {
         
-    separableTransformNet = await tf.loadGraphModel('./models/saved_model_transformer_separable_js/model.json');
-    return separableTransformNet;
-}
+        mobileStyleNet = await tf.loadGraphModel('./models/saved_model_style_js/model.json');
+        return mobileStyleNet;
+    }
+
+    async  loadInceptionStyleModel() {
+        
+        inceptionStyleNet = await tf.loadGraphModel('./models/saved_model_style_inception_js/model.json');
+        return inceptionStyleNet;
+    }
+
+    async  loadOriginalTransformerModel() {
+        
+        originalTransformNet = await tf.loadGraphModel('./models/saved_model_transformer_js/model.json');
+        return originalTransformNet;
+    }
+
+    async  loadSeparableTransformerModel() {
+            
+        separableTransformNet = await tf.loadGraphModel('./models/saved_model_transformer_separable_js/model.json');
+        return separableTransformNet;
+    }
+
+    startStyling(content,style){
+        return this.styleNet.predict(tf.browser.fromPixels(style).toFloat().div(tf.scalar(255)).expandDims());
+    }
 }
